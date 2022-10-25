2ed7a3e316bbe4a5a4558062d900501d2f87c992
Jithru Jayawantha <78858779+Jithru@users.noreply.github.com>
2022-10-25 18:26:46 +0530

    map 3 backend

--- Serverside/config/db.config.js
+++ Serverside/config/db.config.js
@@ -2,12 +2,20 @@ const mysql = require('mysql');
 
 // create here mysql connection
 
+// const dbConn = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',    
+//     password: '',
+//     database: 'slhandyfix_db'
+// });
+
 const dbConn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'slhandyfix_db'
+    host: 'sql6.freesqldatabase.com',
+    user: 'sql6528677',
+    password: '8Y2T8K1vfE',
+    database: 'sql6528677'
 });
+
 
 dbConn.connect(function(error){
     if(error) throw error;
