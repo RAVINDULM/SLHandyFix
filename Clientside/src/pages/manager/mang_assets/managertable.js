5c86b4e146dde037402014bb45fe3685bbb2b959
Rumesha Disanayaka <2019cs017@stu.ucsc.lk>
2022-10-25 20:37:31 +0530

    Update services

--- Clientside/src/pages/manager/mang_assets/managertable.js
+++ Clientside/src/pages/manager/mang_assets/managertable.js
@@ -19,43 +19,31 @@ import { DocsCallout, DocsExample } from "src/components";
 import { CButton } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
 import { cilBell } from "@coreui/icons";
+import { Link, useParams } from "react-router-dom";
 
-const Table = ({data,column}) => {
+
+
+const Table = ({data, setServiceDetails, column}) => {
+
+  const [id, setId] = useState({});
+  const params = useParams();
+ 
+
+  const Abc = async (itemId) =>{
+    try{
+      console.log(itemId)
+;      const data = await Axios.delete(`http://localhost:5000/api/v1/service/deleteService/${itemId}`);
+      if(data.status === 200){
+        setServiceDetails( data => data.filter( (item) => item.id !== itemId ))
+      }
+      
+    }catch(err){
+      console.error(err.message);
+    }
+   }
+
   return (
-    <div>  
-
-{/* <DocsExample href="components/buttons">
-              {['normal', 'active', 'disabled'].map((state, index) => (
-                <CRow className="align-items-center mb-3" key={index}>
-                  <CCol xs={12} xl={2} className="mb-3 mb-xl-0">
-                    {state.charAt(0).toUpperCase() + state.slice(1)}
-                  </CCol>
-                  <CCol xs>
-                    {[
-                      'primary',
-                      'secondary',
-                      'success',
-                      'danger',
-                      'warning',
-                      'info',
-                      'light',
-                      'dark',
-                    ].map((color, index) => (
-                      <CButton
-                        color={color}
-                        key={index}
-                        active={state === 'active'}
-                        disabled={state === 'disabled'}
-                      >
-                        {color.charAt(0).toUpperCase() + color.slice(1)}
-                      </CButton>
-                    ))}
-                    <CButton color="link">Link</CButton>
-                  </CCol>
-                </CRow>
-              ))}
-            </DocsExample> */}
-         
+    <div>   
 
       <CCol xs={12}>
         <CCard className="mb-4">
@@ -70,7 +58,7 @@ const Table = ({data,column}) => {
                   </CTableRow>
                 </CTableHead>
                 <CTableBody>
-                  {Object.values(data).map((item,index) => <TableRow item = {item} column ={column}/>)}                 
+                  {Object.values(data).map((item,index) => <TableRow item = {item} column ={column} Abc = {Abc}/>)}                 
                 </CTableBody>
               </CTable>
           </CCardBody>
@@ -82,17 +70,18 @@ const Table = ({data,column}) => {
 
 const TableHead =({ item }) => <CTableHeaderCell scope="col">{item.Heading}</CTableHeaderCell>
 
-const TableRow = ({ item , column }) => (
+const TableRow = ({ item , column, Abc }) => (
     
     <CTableRow key={item[`id`]} >
-        {console.log(item[`id`])}
+        {console.log(item.id)}
         {/* {column.map((columnItem, index)=>{ return <CTableDataCell key={item[`id`]}>{item[`${columnItem.value}`]}</CTableDataCell>})} */}
         {column.map((columnItem, index)=>{ {if(columnItem.value != "actions") {return  (<CTableDataCell>{item[`${columnItem.value}`]}</CTableDataCell>)}
         else{
            return (
             <CTableDataCell> 
-              <CCol>
-                <CButton color="warning" key={index}>Remove</CButton>
+              <CCol>{item.numofemployeess!=0?<></>:<CButton color="warning" key={index} onClick={ () => Abc(item.id)}>Remove</CButton>}
+
+                
                 {/* <CButton color="danger" key={index}>Check</CButton> */}
               </CCol>
               </CTableDataCell>)}
@@ -100,4 +89,6 @@ const TableRow = ({ item , column }) => (
        
     </CTableRow>
 )
-export default Table;
+
+  
+  export default Table;
